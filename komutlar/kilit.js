5722ff89d90451be6ff6581fa8c68881beda3a9f
Glitch (invincible-sugary-aardwolf) <none>
2020-05-08 16:30:22 +0000

    🎥📠 Checkpoint ./komutlar/kilit.js:3756271/2793

--- komutlar/kilit.js
+++ komutlar/kilit.js
@@ -1,31 +1,52 @@
 const Discord = require('discord.js');
-
-exports.run = async (codeAcademy, message, args) => {
-  if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("Bu komutu kullanabilmek için **Kanalları Yönet** iznine sahip olmalısın!");
-  let yashinu = message.guild.roles.find(a => a.name === "@everyone");
-  if(message.channel.permissionsFor(yashinu).has('SEND_MESSAGES')) {
-    message.channel.overwritePermissions(yashinu, {
-      SEND_MESSAGES: false,
+const ms = require('ms');
+exports.run = (client, message, args) => {
+  if (!message.guild) {
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', '`kilit` adlı komutu özel mesajlarda kullanamazsın.')
+  return message.author.sendEmbed(ozelmesajuyari); }
+  if (!client.lockit) client.lockit = [];
+  let time = args.join(' ');
+  let validUnlocks = ['kilitaç', 'unlock'];
+  if (!time) return message.channel.send('**Doğru Kullanım** : `!kilitle 2min`');
+  if (validUnlocks.includes(time)) {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: null
+    }).then(() => {
+      message.channel.send('**Kanal Kilidi Açıldı.**');
+      clearTimeout(client.lockit[message.channel.id]);
+      delete client.lockit[message.channel.id];
+    }).catch(error => {
+      console.log(error);
     });
-    message.channel.send('Kanal kilitlendi!')
   } else {
-    message.channel.overwritePermissions(yashinu, {
-      SEND_MESSAGES: null,
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: false
+    }).then(() => {
+      message.channel.send(`**Kanal Kilitlendi.** ${ms(ms(time), { long:true })}`).then(() => {
+        client.lockit[message.channel.id] = setTimeout(() => {
+          message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: null
+          }).then(message.channel.send('**Kanalın Kilidi Açıldı.**')).catch(console.error);
+          delete client.lockit[message.channel.id];
+        }, ms(time));
+      }).catch(error => {
+        console.log(error);
+      });
     });
-    message.channel.send('Kanal kilidi açıldı!')
-  };
+  }
 };
-
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['lock'],
-  permLevel: 0
+  aliases: ['kilitle'],
+  permLevel: 3
 };
-
 exports.help = {
-  name: 'kilit',
-  description: 'Kanalı kilitler.',
-  usage: 'kilit',
-  kategori: 'yetkili'
+  name: 'kanalıkilitle',
+  description: 'Kanalı istediğiniz kadar süreyle kitler.',
+  usage: 'kanalıkilitle'
 };
